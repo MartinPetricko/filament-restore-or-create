3e8f4d3e54829bfd92c92cc64b12f0a56ba360db
Martin Petricko <matopetricko@gmail.com>
2025-04-16 11:21:57 +0200

    Initial commit

---
+++ src/FilamentRestoreOrCreateServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MartinPetricko\FilamentRestoreOrCreate;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentRestoreOrCreateServiceProvider extends PackageServiceProvider
+{
+    public static string $name = 'filament-restore-or-create';
+
+    public function configurePackage(Package $package): void
+    {
+        $package->name(static::$name)
+            ->hasTranslations()
+            ->hasViews();
+    }
+
+    public function packageRegistered(): void
+    {
+        //
+    }
+
+    public function packageBooted(): void
+    {
+        //
+    }
+}
